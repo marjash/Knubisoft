d37e5a86d8f76f986ba33da0bbd69be74c452bce
Oksana <marjash@i.ua>
2022-07-25 21:52:58 +0300

    added numbers

--- src/main/java/com/knubisoft/base/numbers/NumbersTasksImpl.java
+++ src/main/java/com/knubisoft/base/numbers/NumbersTasksImpl.java
@@ -1,9 +1,11 @@
 package com.knubisoft.base.numbers;
 
+import javax.xml.crypto.Data;
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 public class NumbersTasksImpl implements NumbersTasks {
     public static void main(String[] args) {
@@ -75,27 +77,83 @@ public class NumbersTasksImpl implements NumbersTasks {
 
     @Override
     public boolean isPrime(int number) {
-        return false;
+        boolean b = false;
+        for (int i = 2; i < number / 2; i++) {
+            if (number % i == 0)
+                return false;
+            else
+                b = true;
+        }
+        return b;
     }
 
     @Override
     public boolean isArmstrongNumber(int number) {
-        return false;
+        String s = String.valueOf(number);
+        int num = number;
+        double n = 0;
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            n = number % 10;
+            n = Math.pow(n, 3);
+            sum += n;
+            number = number / 10;
+        }
+        return sum == num;
     }
 
     @Override
     public BigInteger factorial(int number) {
-        return BigInteger.ZERO;
+        BigInteger bigInteger = BigInteger.valueOf(1);
+        if (number == 0)
+            return BigInteger.valueOf(1);
+        else
+            return factorial(number - 1).multiply(BigInteger.valueOf(number));
     }
 
     @Override
     public boolean palindrome(int number) {
-        return false;
+        String s = String.valueOf(number);
+        if (s.length() == 1)
+            return true;
+        int[] num = new int[s.length()];
+        int n = 0;
+        boolean b = false;
+        for (int i = 0; i < s.length(); i++) {
+            n = number % 10;
+            num[i] = n;
+            number = number / 10;
+        }
+        for (int i = 0; i < num.length / 2; i++) {
+            if (num[i] == num[num.length - 1 - i]){
+                b = true;
+            }
+            else {
+                return false;
+            }
+        }
+        return b;
     }
 
     @Override
     public boolean isAutomorphic(int number) {
-        return false;
+        long num = (long) number * number;
+        String str = String.valueOf(number);
+//        char[] ch = str.toCharArray();
+        String str2 = String.valueOf(num);
+//        char[] ch2 = str2.toCharArray();
+        boolean b = false;
+        int count = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+//            System.out.println("num " + str2.charAt(i) + " number " + str.charAt(str.length() - 1 - count));
+            if (str.charAt(i) == str2.charAt(str2.length() - 1 - count)){
+                b = true;
+                count++;
+            }
+            else
+                return false;
+        }
+        return b;
     }
 
 }
