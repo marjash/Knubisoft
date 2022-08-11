4180c8b423a714a007c0e83a9eed35de80e08e1e
Oksana <marjash@i.ua>
2022-08-11 19:59:15 +0300

    added string

--- src/main/java/com/knubisoft/base/string/StringTasksImpl.java
+++ src/main/java/com/knubisoft/base/string/StringTasksImpl.java
@@ -1,12 +1,16 @@
 package com.knubisoft.base.string;
 
 import java.util.Locale;
+import java.util.NoSuchElementException;
 
 public class StringTasksImpl implements StringTasks {
     private String str = "Java";
 
     @Override
     public String reverseString(String original) {
+        if (original == null){
+            throw new IllegalArgumentException();
+        }
         String str = "";
         for (int i = original.length() - 1; i >= 0; i--) {
             str += original.charAt(i);
@@ -16,6 +20,9 @@ public class StringTasksImpl implements StringTasks {
 
     @Override
     public String insertStringInMiddle(String original, String toInsert) {
+        if (original == null || toInsert == null || original.equals("") || toInsert.equals("")){
+            throw new IllegalArgumentException();
+        }
         String substring = original.substring(0, (original.length() / 2));
         String substring2 = original.substring((original.length() / 2));
         String result = substring + toInsert + substring2;
@@ -24,26 +31,57 @@ public class StringTasksImpl implements StringTasks {
 
     @Override
     public String insertSymbolInString(String original, char toInsert, int position) {
-        return null;
+        if (original == null || original.equals("") || position < 0 || position > original.length()){
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < original.length(); i++) {
+            if (i == position){
+                String substring = original.substring(0, position);
+                String substring2 = original.substring(position);
+                original = substring + toInsert + substring2;
+            }
+        }
+        return original;
     }
 
     @Override
     public String appendToString(StringBuilder original, String toAppend) {
-        return null;
+        if (original == null || toAppend == null || original.toString().equals("") || toAppend.equals("")){
+            throw new NoSuchElementException();
+        }
+        return original.append(toAppend).toString();
     }
 
     @Override
     public boolean isPalindrome(String palindrome) {
-        return false;
+        if (palindrome == null)
+            throw new RuntimeException();
+        if (palindrome.equals(""))
+            return true;
+        for (int i = 0; i < palindrome.length() / 2; i++) {
+            if (!(palindrome.charAt(i) == palindrome.charAt(palindrome.length() - 1 - i)))
+                return false;
+        }
+        return true;
     }
 
     @Override
     public boolean hasLowerCase(String str) {
-        return false;
+        if (str == null){
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isUpperCase(c))
+                return false;
+        }
+        return true;
     }
 
     @Override
     public String uniqueCharacters(String str) {
+        if (str == null)
+            throw  new IllegalArgumentException();
         boolean b = false;
         String res = "";
         str = str.toLowerCase();
