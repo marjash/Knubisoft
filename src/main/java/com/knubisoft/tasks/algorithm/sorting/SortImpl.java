d099f1ef096df6e1ed66959c61912fee49bb84a1
Oksana <marjash@i.ua>
2022-09-29 14:28:09 +0300

    add sort

--- src/main/java/com/knubisoft/tasks/algorithm/sorting/SortImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/sorting/SortImpl.java
@@ -1,13 +1,17 @@
 package com.knubisoft.tasks.algorithm.sorting;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SortImpl implements Sort {
 
     @Override
     public Map<Integer, List<Integer>> sortKeysAndValues(Map<Integer, List<Integer>> input) {
-        return null;
+        Map<Integer, List<Integer>> map = new TreeMap<>(input);
+        Set<Integer> integers = map.keySet();
+        for (int key : integers) {
+            List<Integer> list = map.get(key);
+            list.sort(Comparator.naturalOrder());
+        }
+        return map;
     }
-
 }
