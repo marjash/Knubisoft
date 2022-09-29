83aa26c6a8558943c0212c1c781c524ca1e9f2c9
Oksana <marjash@i.ua>
2022-09-29 13:15:36 +0300

    add lucky ticket

--- src/main/java/com/knubisoft/tasks/algorithm/json/JsonTasksImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/json/JsonTasksImpl.java
@@ -70,5 +70,4 @@ public class JsonTasksImpl implements JsonTasks {
         }
         return list;
     }
-
 }
--- src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
@@ -3,6 +3,18 @@ package com.knubisoft.tasks.algorithm.luckyticket;
 public class LuckyTicketImpl implements LuckyTicket {
     @Override
     public boolean checkIsLuckyTicket(String ticket) {
-        return false;
+        if (ticket == null || ticket.length() < 6 || ticket.length() > 8)
+            return false;
+        try {
+            Integer.parseInt(ticket);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        for (int i = 0; i < ticket.length() / 2; i++) {
+            if (!(ticket.charAt(i) == ticket.charAt(ticket.length() -1 - i)))
+                return false;
+        }
+        return true;
     }
 }
